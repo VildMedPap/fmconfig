d26dd1107df822ad21a1b968de26cdeb164dcf50
Sebastian Steenssøe <vildmedpap@gmail.com>
2024-05-22 22:38:54 +0200

    test: add monkeypatch

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import pytest
 from typer.testing import CliRunner
 
 from fmconfig.cli import app
@@ -5,7 +6,17 @@ from fmconfig.cli import app
 runner = CliRunner()
 
 
-def test_app():
+def test_app(monkeypatch: pytest.MonkeyPatch):
+    # Given
+    # MacOS system
+    expected = "Darwin"
+    monkeypatch.setattr("platform.system", lambda: expected)
+
+    # When
+    # Invoking the app
     result = runner.invoke(app)
+
+    # Then
+    # It should run as expected
     assert result.exit_code == 0
     assert "Football Manager" in result.stdout
